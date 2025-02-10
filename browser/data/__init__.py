079e83dbd3decf5335906dd5b206209f30516355
david <dav94sal@gmail.com>
2025-02-10 12:21:14 -0600

    Remove browser application for independent git repo

--- browser/data/__init__.py
+++
@@ -1,2 +0,0 @@
-from .mental_health_data import mental_health_urls
-from .job_seeking_data import job_urls
--- browser/data/mental_health_data.py
+++
@@ -1,4 +0,0 @@
-mental_health_urls = [
-    "https://docs.google.com/document/d/1y6-LV9h100EzaoJvVpzRfKRiHtfDMw8Ce1jVPi9xgzI/edit?tab=t.0",
-    "https://docs.google.com/document/d/16M6FZE4LLRqVZ2ND1IMgdN9JiF6maQ3jyjkDnoyxOtk/edit?tab=t.0",
-]
